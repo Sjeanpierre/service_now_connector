82267714935bbba82273fa79686ce92155f8f8cc
Stevenson Jean-Pierre <Stevenson.Jean-Pierre@sage.com>
2017-04-16 00:40:08 -0400

    Api refactor (#2)
    * more structural changes
    
    * renamed

--- main.go
+++ main.go
@@ -5,25 +5,19 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/gorilla/handlers"
 	"net/http"
+	"github.com/sjeanpierre/service_now_proxy/lib/servicenow/snclient"
+	"github.com/sjeanpierre/service_now_proxy/lib/servicenow/snapi"
 	"os"
-	"github.com/sjeanpierre/service_now_connector/lib/servicenow/snclient"
 )
 
 
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
-	registerHandlers(router)
+	snapi.RegisterHandlers(router)
 	loggedRouter := handlers.LoggingHandler(os.Stdout, router)
-	log.Println("Validating Config")
+	log.Println("Validating Service Now proxy Config")
 	snclient.NewClient() //used to check if environment variables are defined
 	log.Println("Started: Ready to serve")
 	log.Fatal(http.ListenAndServe(":8080", loggedRouter)) //todo, refactor to make port dynamic
-}
-
-func registerHandlers(r *mux.Router)  {
-	r.NotFoundHandler = http.HandlerFunc(notFoundHandler)
-	r.HandleFunc(`/incidents/{incident:INC\d{7,10}}`, IncidentHandler)
-	r.HandleFunc("/incidents/{option:count}/{team}", IncidentTeamHandler)
-	r.HandleFunc("/incidents/{option:list}/{team}", IncidentTeamHandler)
 }
